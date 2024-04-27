653e2d12e3c4dc3598f68418efa657c23df9608c
Thiago Moraes <thimoraesadm@gmail.com>
2024-04-27 01:59:25 -0300

    add recurso de modificação de dados

--- src/main/java/med/revo/api/controller/MedicoController.java
+++ src/main/java/med/revo/api/controller/MedicoController.java
@@ -3,13 +3,14 @@ package med.revo.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.revo.api.medico.DadosCadastroMedicos;
+import med.revo.api.medico.DadosListagemMedicos;
 import med.revo.api.medico.Medico;
 import med.revo.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -24,4 +25,8 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
 
+    @GetMapping
+    public List<DadosListagemMedicos> listar() {
+        return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
+    }
 }
